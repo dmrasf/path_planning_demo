1e6b7837bf105ae72bf0421a51d62ef7101e7d48
dmrasf <dmrasf@qq.com>
2021-03-25 23:59:28 +0800

    A*算法改进

--- lib/components/show_map_a.dart
+++ lib/components/show_map_a.dart
@@ -22,9 +22,12 @@ class ShowMapForAState extends State<ShowMapForA>
   AnimationController _controller;
   List<dynamic> _visualPoints = [];
   List<dynamic> _visualGraph;
-  List<dynamic> _openPoints = [];
-  List<double> _openPointsValue = [];
-  List<dynamic> _closePoints = [];
+  Set _openPoints = Set();
+  Set _closePoints = Set();
+  Map<int, int> _tree = Map<int, int>();
+  Map<int, double> _pointToStartDis = Map<int, double>();
+  Map<int, double> _openPointsValue = Map<int, double>();
+  bool _isOp = true;
   List<int> _pathRoute = [];
   List<int> _pathRouteOp = [];
   bool _isShowOp = false;
@@ -63,6 +66,7 @@ class ShowMapForAState extends State<ShowMapForA>
                   this._visualPoints,
                   this._openPoints,
                   this._openPointsValue,
+                  this._tree,
                   this._closePoints,
                   this._pathRoute,
                   this._pathRouteOp,
@@ -141,42 +145,33 @@ class ShowMapForAState extends State<ShowMapForA>
     }
     _controller.reset();
     _controller.forward();
-    _openPoints = [];
-    _openPointsValue = [];
-    _closePoints = [];
-    _pathRoute = [];
-    _pathRouteOp = [];
+    _closePoints.clear();
+    _openPoints.clear();
+    _openPointsValue.clear();
+    _tree.clear();
+    _pathRoute.clear();
+    _pathRouteOp.clear();
     _i = 0;
     int start = 0;
     int end = _visualPoints.length - 1;
-    List<int> currentPoint = [start, start];
+    int currentPoint = start;
     _closePoints.add(currentPoint);
+
     while (true) {
       _updateOpenPoints(currentPoint);
       currentPoint = _findNextPoint(currentPoint, hWeight, gWeight);
       _state = 'Update close set';
       await Future.delayed(Duration(milliseconds: _speed));
-      try {
-        bool isBreak = false;
-        int i = 0;
-        for (; i < _openPoints.length; i++)
-          if (_openPoints[i][0] == currentPoint[0]) {
-            isBreak = true;
-            break;
-          }
-        if (!isBreak) throw UnimplementedError();
-        _openPoints.removeAt(i);
-      } catch (e) {
+      if (!_openPoints.remove(currentPoint)) {
         _state = 'Failure, No path !';
         return;
       }
-      if (!_closePoints.contains(currentPoint)) _closePoints.add(currentPoint);
+      _closePoints.add(currentPoint);
       _state = 'Update open set & Find next positon';
       await Future.delayed(Duration(milliseconds: _speed));
-      if (currentPoint[0] == end) break;
+      if (currentPoint == end) break;
     }
     _state = 'Success !';
-    _closePoints.remove([start, start]);
     _parsePathRoute();
     _optimisingPath();
     (showPathDiatance.currentState as ShowPathDistanceState).update(
@@ -225,80 +220,95 @@ class ShowMapForAState extends State<ShowMapForA>
     return pathDistance;
   }
 
-  List<int> _findNextPoint(
-    List<int> currentPoint,
+  int _findNextPoint(
+    int currentPoint,
     double hWeight,
     double gWeight,
   ) {
-    _openPointsValue = [];
+    _openPointsValue.clear();
     double minDistance = double.infinity;
-    List<int> bestPoint = [];
+    int bestPoint;
     int end = _visualPoints.length - 1;
     double grid = _myMap['grid'];
-    for (int i = 0; i < _openPoints.length; i++) {
-      List<int> tmpPoints = _openPoints[i];
+    for (int p in _openPoints) {
       double h = pow(
-        pow((_visualPoints[end][0] - _visualPoints[tmpPoints[0]][0]) * grid,
-                2) +
-            pow((_visualPoints[end][1] - _visualPoints[tmpPoints[0]][1]) * grid,
-                2),
+        pow((_visualPoints[end][0] - _visualPoints[p][0]) * grid, 2) +
+            pow((_visualPoints[end][1] - _visualPoints[p][1]) * grid, 2),
         0.5,
       );
-      double g = _visualGraph[currentPoint[0]][tmpPoints[0]];
+      double g = _visualGraph[currentPoint][p];
       double f =
           h * (hWeight == 0 ? 1 : hWeight) + g * (gWeight == 0 ? 1 : gWeight);
-      _openPointsValue.add(f);
+      _openPointsValue[p] = f;
       if (f < minDistance) {
         minDistance = f;
-        bestPoint.clear();
-        bestPoint.add(tmpPoints[0]);
-        bestPoint.add(tmpPoints[1]);
+        bestPoint = p;
       }
     }
     return bestPoint;
   }
 
-  void _updateOpenPoints(List<int> currentPoint) {
+  void _updateOpenPoints(int currentPoint) {
+    List<int> newPoint = [];
     for (int i = 0; i < _visualPoints.length; i++) {
-      if (_visualGraph[currentPoint[0]][i] == -1.0) continue;
-      bool isBreak = false;
-      for (int j = 0; j < _closePoints.length; j++)
-        if (i == _closePoints[j][0]) {
-          isBreak = true;
-          break;
-        }
-      if (!isBreak) {
-        isBreak = false;
-        for (int k = 0; k < _openPoints.length; k++)
-          if (i == _openPoints[k][0]) {
-            isBreak = true;
-            break;
-          }
-        if (!isBreak) _openPoints.add([i, currentPoint[0]]);
+      if (_visualGraph[currentPoint][i] > 0 &&
+          !_openPoints.contains(i) &&
+          !_closePoints.contains(i)) {
+        _tree[i] = currentPoint;
+        _openPoints.add(i);
+        newPoint.add(i);
+      }
+    }
+    _openPoints.forEach((e) => _changeParent(e));
+  }
+
+  void _changeParent(int opPoint) {
+    double distance = double.infinity;
+    for (int parent in _closePoints.union(_openPoints)) {
+      double tmp = _visualGraph[parent][opPoint];
+      if (tmp <= 0) continue;
+      if (tmp + _calculatePathDis(parent) < distance) {
+        distance = tmp + _calculatePathDis(parent);
+        _tree[opPoint] = parent;
       }
     }
   }
 
+  double _calculatePathDis(int p) {
+    if (!_pointToStartDis.containsKey(p)) {
+      double dis = 0;
+      List<int> path = [p];
+      while (path[path.length - 1] != 0) {
+        path.add(_tree[path[path.length - 1]]);
+        dis = dis + _visualGraph[path[path.length - 1]][path[path.length - 2]];
+      }
+      _pointToStartDis[p] = dis;
+    }
+    return _pointToStartDis[p];
+    //double dis = 0;
+    //List<int> path = [p];
+    //while (path[path.length - 1] != 0) {
+    //path.add(_tree[path[path.length - 1]]);
+    //dis = dis + _visualGraph[path[path.length - 1]][path[path.length - 2]];
+    //}
+    //_pointToStartDis[p] = dis;
+    //return _pointToStartDis[p];
+  }
+
   void _parsePathRoute() {
     _pathRoute = [_visualPoints.length - 1];
-    int tmpPoint = _visualPoints.length - 1;
-    while (true) {
-      for (var p in _closePoints) {
-        if (p[0] == tmpPoint) {
-          _pathRoute.add(p[1]);
-          tmpPoint = p[1];
-        }
-      }
-      if (tmpPoint == 0) break;
-    }
+    _pathRoute = [_visualPoints.length - 1];
+    while (_pathRoute[_pathRoute.length - 1] != 0)
+      _pathRoute.add(_tree[_pathRoute[_pathRoute.length - 1]]);
   }
 }
 
 class PainteA extends MapPainter {
   final List<dynamic> _visualPoints;
-  final List<dynamic> _closePoints;
-  final List<dynamic> _openPoints;
-  final List<double> _openPointsValue;
+  final Set _closePoints;
+  final Set _openPoints;
+  final Map<int, double> _openPointsValue;
+  final Map<int, int> _tree;
   final List<int> _pathRoute;
   final String _state;
   final List<int> _pathRouteOp;
@@ -310,6 +320,7 @@ class PainteA extends MapPainter {
     this._visualPoints,
     this._openPoints,
     this._openPointsValue,
+    this._tree,
     this._closePoints,
     this._pathRoute,
     this._pathRouteOp,
@@ -339,26 +350,30 @@ class PainteA extends MapPainter {
           Colors.orange.shade900, _i + 1);
     super.drawRobot(canvas, size, myPaint, k);
     drawSet(canvas, size, myPaint, k);
+    drawTree(canvas, size, myPaint, k);
     super.drawState(canvas, size, myPaint, k, _state);
   }
 
   void drawSet(Canvas canvas, Size size, Paint myPaint, double k) {
-    for (int i = 0; i < _openPoints.length; i++) {
+    for (int p in _openPoints) {
       myPaint..color = Colors.yellow;
       Offset offset = Offset(
-        _visualPoints[_openPoints[i][0]][1].toDouble() * k +
+        _visualPoints[p][1].toDouble() * k +
             (size.width - k * (super.width / super.grid)) / 2,
-        _visualPoints[_openPoints[i][0]][0].toDouble() * k +
+        _visualPoints[p][0].toDouble() * k +
             (size.height - k * (super.heigth / super.grid)) / 2,
       );
       canvas.drawCircle(
           offset, super.robotSize / (super.grid + 0.035) * k, myPaint);
       if (_openPointsValue.isNotEmpty) {
-        double ma = _openPointsValue.reduce(max);
-        double mi = _openPointsValue.reduce(min);
-        double fontSize = k * (7 - 3 * (_openPointsValue[i] - mi) / (ma - mi));
+        //double ma = _openPointsValue.;
+        //double mi = _openPointsValue.reduce(min);
+        double mi = 0;
+        double ma = 10;
+        double fontSize = k * (7 - 3 * (_openPointsValue[p] - mi) / (ma - mi));
+        fontSize = 1;
         TextSpan span = TextSpan(
-          text: _openPointsValue[i].toStringAsFixed(1),
+          text: _openPointsValue[p].toStringAsFixed(1),
           style: GoogleFonts.jua(
             textStyle: TextStyle(
               color: Colors.black,
@@ -376,17 +391,37 @@ class PainteA extends MapPainter {
         tp.paint(canvas, offset + Offset(-fontSize * 0.7, -fontSize * 0.6));
       }
     }
-    for (int i = 0; i < _closePoints.length; i++) {
+    for (int p in _closePoints) {
       myPaint..color = Colors.blue;
       canvas.drawCircle(
         Offset(
-            _visualPoints[_closePoints[i][0]][1].toDouble() * k +
+            _visualPoints[p][1].toDouble() * k +
                 (size.width - k * (super.width / super.grid)) / 2,
-            _visualPoints[_closePoints[i][0]][0].toDouble() * k +
+            _visualPoints[p][0].toDouble() * k +
                 (size.height - k * (super.heigth / super.grid)) / 2),
         super.robotSize / (super.grid + 0.035) * k,
         myPaint,
       );
     }
+  }
+
+  void drawTree(Canvas canvas, Size size, Paint myPaint, double k) {
+    _tree.forEach((c, p) {
+      Offset p1 = Offset(
+          _visualPoints[c][1].toDouble() * k +
+              (size.width - k * (super.width / super.grid)) / 2,
+          _visualPoints[c][0].toDouble() * k +
+              (size.height - k * (super.heigth / super.grid)) / 2);
+      Offset p2 = Offset(
+          _visualPoints[p][1].toDouble() * k +
+              (size.width - k * (super.width / super.grid)) / 2,
+          _visualPoints[p][0].toDouble() * k +
+              (size.height - k * (super.heigth / super.grid)) / 2);
+      myPaint
+        ..strokeWidth = 2
+        ..color = Colors.blue
+        ..strokeCap = StrokeCap.round;
+      canvas.drawLine(p1, p2, myPaint);
+    });
   }
 }
